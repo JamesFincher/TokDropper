e164da99be7000de4912dc686c562abdc5eb08dd
James Fincher <james@Jamess-MacBook-Pro.local>
2023-01-06 09:31:47 -0800

    created dash folder, and fixed imports for admin in new directory

--- src/pages/dash/admin.tsx
+++ src/pages/dash/admin.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+import { trpc } from "../../utils/trpc";
 
-import { trpc } from "../utils/trpc";
 
 const Admin: React.FC = () => {
   const { data: sessionData } = useSession();
